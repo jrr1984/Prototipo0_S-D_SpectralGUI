33f1b48be687a5cea9930b5e0183e04d9cfc86bc
Juan Reto Reynal <juanreto@gmail.com>
2019-05-24 19:42:58 -0300

    wait bien implementado

--- syst/thor_stepm.py
+++ syst/thor_stepm.py
@@ -92,9 +92,6 @@ class ThorlabsStageWithStepMotors:
         motor.clear_message_queue()
         message_type, message_id, _ = motor.wait_for_message()
         while message_type != 2 or message_id != value:
-            position = motor.get_position()
-            real = motor.get_real_value_from_device_unit(position, 'DISTANCE')
-            #print('  at position {} [device units] {} [real-world units]'.format(position, real))
             message_type, message_id, _ = motor.wait_for_message()
             
     def move_to_x_y(self,x,y):
