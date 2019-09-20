fb9d64231bf632b4b0fccda94587c1cd48b75f38
Juan Reto Reynal <juanreto@gmail.com>
2019-09-20 20:15:42 -0300

    fin del dia

--- barrido/barrido_sin_threads/ccs_tests.py
+++ barrido/barrido_sin_threads/ccs_tests.py
@@ -1,12 +1,16 @@
 import visa
-import time
+import numpy as np
+import time, csv
 from instrumental import instrument,list_instruments,u, Q_
 from instrumental.drivers.spectrometers.thorlabs_ccs import CCS
 paramsets = list_instruments()
 ccs = instrument(paramsets[0])
 
 initial_time = time.time()
-ccs.take_data(integration_time=None, num_avg=5, use_background=False)
+inten, wavel = ccs.take_data(integration_time=None, num_avg=5, use_background=False)
+
+np.savetxt('out.csv',wavel,delimiter=',')
+
 elapsed_time = time.time() - initial_time
 print(elapsed_time)
 ccs.close()
--- barrido/std/main.py
+++ barrido/std/main.py
@@ -1,6 +1,7 @@
 import logging,threading,time
 from logging.handlers import SocketHandler
-from std.StageAndSpec import System
+#from std.StageAndSpec import System
+from StageAndSpec import System
 log = logging.getLogger('Root logger')
 log.setLevel(1)  # to send all messages to cutelog
 socket_handler = SocketHandler('127.0.0.1', 19996)  # default listening address
@@ -17,7 +18,6 @@ dy = 500.0
 # y_array_scan = np.arange(0.0,dy*(25+1),dy)
 x_array_scan = np.arange(0.0,dx*(5+1),dx)
 y_array_scan = np.arange(0.0,dy*(5+1),dy)
-
 BE_thread = threading.Thread(target=syst.scan_meander, args=(x_array_scan,y_array_scan,num_avg))
 BE_thread.start()
 storage_thread = threading.Thread(target = syst.storage_thread, args=(BE_thread,))
