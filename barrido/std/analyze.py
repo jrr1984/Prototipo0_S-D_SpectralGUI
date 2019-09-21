0b5dfdfb861b128c138b898f087d33b85ff394e6
Juan Reto Reynal <juanreto@gmail.com>
2019-09-21 16:14:56 -0300

    actualizacion cuaderno lab

--- barrido/std/analyze.py
+++ barrido/std/analyze.py
@@ -3,16 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('QT4Agg')
 
-wavel_file = np.loadtxt('long_de_onda.txt', delimiter=',')
-wavel = wavel_file[0,:]
+#wavel_file = np.loadtxt('long_de_onda.txt', delimiter=',')
+#wavel = wavel_file[0,:]
 #print(np.size(wavel))
+wavel = pd.read_csv('long_de_onda_1_tira.csv')
+print(type(wavel))
+# print(wavel)
 inten = pd.read_csv('inten.csv')
 #print(inten)
-# for i in range(np.size(wavel)):
-#     plt.plot(wavel,inten.iloc[i])
-#     figManager = plt.get_current_fig_manager()
-#     figManager.window.showMaximized()
-#     plt.show()
+for i in range(np.size(wavel)):
+    plt.plot(wavel.iloc[:,0],inten.iloc[i])
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
+    plt.show()
 
-sp1 = np.array([wavel,inten.iloc[0]])
-print(len(sp1))
+# sp1 = np.array([wavel,inten.iloc[0]])
+# print(len(sp1))
