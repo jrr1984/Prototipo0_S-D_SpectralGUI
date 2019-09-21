d37cfcd4c3d88b29d723759882870b416a9fe796
Juan Reto Reynal <juanreto@gmail.com>
2019-09-21 14:46:53 -0300

    agregamos cuaderno lab

---
+++ barrido/learning.py
@@ -0,0 +1,6 @@
+import numpy as np
+import time, sys
+onedima = np.array([1,2,3])
+twodima = np.array([(1,2,3),(4,5,6),(0,0,0),(0,0,0)])
+#print(twodima)
+print(twodima.shape)
--- barrido/std/main.py
+++ barrido/std/main.py
@@ -1,7 +1,6 @@
 import logging,threading,time
 from logging.handlers import SocketHandler
-#from std.StageAndSpec import System
-from StageAndSpec import System
+from std.StageAndSpec import System
 log = logging.getLogger('Root logger')
 log.setLevel(1)  # to send all messages to cutelog
 socket_handler = SocketHandler('127.0.0.1', 19996)  # default listening address
