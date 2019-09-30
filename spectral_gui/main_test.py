10a2139bb7bafe5764f5b1726715b46020ddc82b
Juan Reto Reynal <juanreto@gmail.com>
2019-09-30 17:59:50 -0300

    spectral_gui starts..

---
+++ spectral_gui/main_test.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Spectral GUI')
+root.iconbitmap('c:/users/juanr/downloads/logo_lec.ico')
+
+
+# myLabel1 = tk.Label(root,text = "Spectral GUI").grid(row=0,column=0)
+# myLabel2 = tk.Label(root,text = "Juan Reto Reynal").grid(row=1,column=1)
+# def myclick():
+#     myLabel = tk.Label(root, text="Juan Reto Reynal")
+#     myLabel.pack()
+#
+# myButton = tk.Button(root,text='click me!',command=myclick)
+# myButton.pack()
+
+root.mainloop()
