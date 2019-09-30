0854fa5804f09e87e4e67b40f7f98e16412fc447
Juan Reto Reynal <juanreto@gmail.com>
2019-09-30 19:27:38 -0300

    finished 5th tutorial sentdx

--- spectral_gui/spectral_gui.py
+++ spectral_gui/spectral_gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -7,10 +8,11 @@ class SpectralGui(tk.Tk):
     def __init__(self,*args,**kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
+        tk.Tk.wm_title(self,"Spectral GUI")
+        tk.Tk.iconbitmap(self,'c:/users/juanr/downloads/logo_lec.ico')
+
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both",expand=True)
-
         container.grid_rowconfigure(0,weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -40,11 +42,11 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text = "Start Page", font = LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text = "Visit Page 1",
+        button = ttk.Button(self, text = "Visit Page 1",
                             command = lambda: controller.show_frame(PageOne))
-        button1.pack()
+        button.pack()
 
-        button1 = tk.Button(self, text="Visit Page 2",
+        button1 = ttk.Button(self, text="Visit Page 2",
                             command=lambda: controller.show_frame(PageTwo))
         button1.pack()
 
@@ -57,11 +59,11 @@ class PageOne(tk.Frame):
         label = tk.Label(self, text="Page One", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button = tk.Button(self, text="Back to Home",
+        button = ttk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button.pack()
 
-        button2 = tk.Button(self, text="Page Two",
+        button2 = ttk.Button(self, text="Page Two",
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
@@ -72,11 +74,11 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button = tk.Button(self, text="Back to Home",
+        button = ttk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button.pack()
 
-        button2 = tk.Button(self, text="Page One",
+        button2 = ttk.Button(self, text="Page One",
                            command=lambda: controller.show_frame(PageOne))
         button2.pack()
 
