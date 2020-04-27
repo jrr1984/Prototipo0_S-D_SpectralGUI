6bbd87f5685ba4ce1d970cb57493c0290a06007d
Juan Reto Reynal <juanreto@gmail.com>
2020-04-27 17:14:20 -0300

    ok

--- spectral_gui/new_csv_from_another.py
+++ spectral_gui/new_csv_from_another.py
@@ -1,6 +1,5 @@
 import pandas as pd
-
-# Reading out just the data lines and storing into a pandas dataframe should work with this:
+# Reading out just the data lines and storing into a pandas dataframe:
 df = pd.read_csv('xy_positions_FULL50micron.csv', skiprows=231)
 df.to_csv('xy_positions_FULL50micron.csv', index=False)
 
